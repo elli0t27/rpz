f3c7a5864f3a8592f7092e0d88d5d6e7f00024f4
elliot <blueallie3346@gmail.com>
2025-06-28 01:22:54 +0530

    gui added

--- script.js
+++ script.js
@@ -15,57 +15,77 @@ function getCompChoice() {
     }
 }
 
-function getHumanChoice() {
-    choice = prompt("Enter your choice:");
-    return choice.toLowerCase();
-}
 let humanScore = 0
 let compScore = 0
 
-function playRound() {
-    const human = getHumanChoice()
-    const comp = getCompChoice()
-    console.log("computer: "+ comp)
+function playRound(human,comp) {
     if (human == comp) {
         console.log("Draw")
+        result.textContent="Draw"
     }
     else if (human == "rock") {
         if (comp == "paper") {
             console.log("Paper beats Rock, You Lose")
+            result.textContent="Paper beats Rock, You Lose"
             compScore++;
         } else {
             console.log("Rock beats Scissor, You Win")
+            result.textContent=("Rock beats Scissor, You Win")
             humanScore++;
         }
     }
     else if (human == "paper") {
         if (comp == "rock") {
             console.log("Paper beats Rock, You Win")
+            result.textContent=("Paper beats Rock, You Win")
             humanScore++;
         } else {
             console.log("Scissors beats paper, You Lose")
+            result.textContent=("Scissors beats paper, You Lose")
+
             compScore++;
         }
     }
     else if (human == "scissor") {
         if (comp == "paper") {
             console.log("scissor beats paper, You win")
+            result.textContent=("Scissors beats paper, You win")
             humanScore++;
         } else {
             console.log("Rock beats Scissor, You Lose")
+            result.textContent=("rock beats scissors, You Lose")
             compScore++;
         }
     }
-}
+    scoreboard.textContent=`Score:
+    you:${humanScore}
+    computer:${compScore}`
 
-function playGame() {
-    for (i=1;i<=5;i++){
-        console.log("Round: "+ i)
-        playRound();
-    }
-    console.log("Scores are:")
-    console.log("You: "+ humanScore)
-    console.log("computer: "+ compScore)
 }
+function scoreCheck(){
+    if(humanScore==5) {
+        alert("YOU WIN")
+        location.reload()}
+    if(compScore==5) {
+        alert("you lose")
+        location.reload()}
+    
+}
+const btn= document.querySelectorAll(".btn")
+btn.forEach((button)=>{
+    button.addEventListener("click",()=>{
+        const playerSelection=button.id
+        const compSelection = getCompChoice()
+        comchoose.textContent="computer chooses: "+compSelection
+        playRound(playerSelection,compSelection)
+        scoreCheck()
+    })
+})
 
-playGame()
+const container= document.querySelector(".container")
+const result= document.createElement("div")
+const comchoose= document.createElement("p")
+const scoreboard=document.createElement("div")
+container.appendChild(comchoose)
+container.appendChild(result)
+container.appendChild(scoreboard)
